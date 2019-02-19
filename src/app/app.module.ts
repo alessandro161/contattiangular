8fe0a80a18fd233284650d4dd925a4f2ce80a611
alessandro161 <alessandrogasparini1995@gmail.com>
2019-02-19 12:33:46 +0100

    il mio secondo commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContattiComponent } from './contatti/contatti.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContattiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/contatti/contatti.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contatti',
+  templateUrl: './contatti.component.html',
+  styleUrls: ['./contatti.component.css']
+})
+export class ContattiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+users=[
+  {title:"alessandro"},
+  {title:"marco"},
+  {title:"andrea"}
+]
+
+}
